1f3024d6a752ef4a4bd9fd6139eca7d27338b88c
marklum7 <vapilin777@gmail.com>
2023-11-01 14:31:08 +0300

    223456789

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 
@@ -17,18 +18,48 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     static ArrayList<String> notes = new ArrayList<String>();
+    static ArrayList<String> notes2 = new ArrayList<String>();
+
     static ArrayAdapter<String> arrayAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = (ListView)findViewById(R.id.listView);
+        ListView listView = findViewById(R.id.listView);
 
-        notes.add("Example Note");
+// определяем строковый массив
+        final ArrayList<String> list = new ArrayList<>();
+
+// используем адаптер данных
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, list);
+        ImageButton button;
+        button = (ImageButton) findViewById(R.id.button);
+        listView.setAdapter(adapter);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                notes.add("Заголовок");
+                notes2.add("Текст");
+                recreate();
+            }
+
+        });
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, notes);
         listView.setAdapter(arrayAdapter);
+
+
+
+
+
+
+
+
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -36,9 +67,11 @@ public class MainActivity extends AppCompatActivity {
             {
                 Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
                 intent.putExtra("noteID", position);            //to tell us which row of listView was tapped
+                intent.putExtra("noteID2", position);
                 startActivity(intent);
             }
         });
+
 
 
 
--- app/src/main/java/com/example/myapplication/NoteEditorActivity.java
+++ app/src/main/java/com/example/myapplication/NoteEditorActivity.java
@@ -6,11 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NoteEditorActivity extends AppCompatActivity {
 
     int noteID;
+
+    int noteID2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,10 @@ public class NoteEditorActivity extends AppCompatActivity {
         EditText editText = (EditText)findViewById(R.id.editText);
         Intent intent = getIntent();
         noteID = intent.getIntExtra("noteID", -1);
+
+        EditText editText2 = (EditText)findViewById(R.id.editText2);
+        Intent intent2 = getIntent();
+        noteID2 = intent2.getIntExtra("noteID2", -1);
 
 
         if(noteID != -1)
@@ -49,6 +64,54 @@ public class NoteEditorActivity extends AppCompatActivity {
 
             }
         });
+
+        if(noteID2 != -1)
+        {
+            editText2.setText(MainActivity.notes2.get(noteID2));
+        }
+
+        editText2.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                MainActivity.notes2.set(noteID2, String.valueOf(s));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+
+            }
+        });
+
+
+
+
+// определяем строковый массив
+
+// используем адаптер данных
+
+        ImageButton button;
+        button = (ImageButton) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                finish();
+            }
+
+        });
+
+
 
     }
 }
